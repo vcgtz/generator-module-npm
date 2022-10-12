60a0aae42dd8248dffd1f37654bde000396155e8
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-10-12 07:19:51 -0500

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'prettier',
+    'airbnb-base',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
