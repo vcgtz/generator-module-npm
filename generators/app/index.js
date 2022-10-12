5ed97a2c8804ab129066b0274eb57a415391f4a3
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-10-12 07:35:13 -0500

    Add inital message

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,12 @@
 const Generator = require('yeoman-generator');
+const yosay = require('yosay');
 
-module.exports = class extends Generator {};
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  initializing() {
+    this.log(yosay('Welcome to the NPM Module generator!'));
+  }
+};
